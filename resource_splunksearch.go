e9aea6ac08be2c648e754b538250a2308956d09a
Joshua Lloret <joshua_lloret@comcast.com>
2019-03-15 14:37:26 -0400

    rm Splunk prefix, add Read/Update, disabled

--- resource_splunksearch.go
+++ resource_splunksearch.go
@@ -9,10 +9,10 @@ import (
 
 func resourceSplunkSearch() *schema.Resource {
 	return &schema.Resource{
-		Create: resourceSplunkSearchCreate,
-		Read:   resourceSplunkSearchRead,
-		Update: resourceSplunkSearchUpdate,
-		Delete: resourceSplunkSearchDelete,
+		Create: resourceSearchCreate,
+		Read:   resourceSearchRead,
+		Update: resourceSearchUpdate,
+		Delete: resourceSearchDelete,
 
 		Schema: map[string]*schema.Schema{
 			"name": &schema.Schema{
@@ -27,12 +27,16 @@ func resourceSplunkSearch() *schema.Resource {
 				Type:	 schema.TypeString,
 				Optional: true,
 			},
+			"disabled": &schema.Schema{
+				Type:	 schema.TypeBool,
+				Optional: true,
+			},
 		},
 	}
 }
 
-func client() spk.SplunkClient {
-	return spk.SplunkClient{
+func client() spk.Client {
+	return spk.Client{
 		Endpoint: os.Getenv("SPLUNK_ENDPOINT"),
 		Username: os.Getenv("SPLUNK_USERNAME"),
 		Password: os.Getenv("SPLUNK_PASSWORD"),
@@ -41,29 +45,61 @@ func client() spk.SplunkClient {
 	}
 }
 
-func resourceSplunkSearchCreate(d *schema.ResourceData, m interface{}) error {
-	sc := client()
-	ss := spk.SplunkSearch{
+func search(d *schema.ResourceData) spk.Search {
+	ss := spk.Search{
 		"name": spk.SType{Str: d.Get("name").(string)},
 		"search": spk.SType{Str: d.Get("search").(string)},
 		"description": spk.SType{Str: d.Get("description").(string)},
 	}
 
-	_, err := sc.NewSearch(ss)
-	return err
+	if (d.Get("disabled").(bool)) {
+		ss["disabled"] = spk.SType{Str: "1"}
+	} else {
+		ss["disabled"] = spk.SType{Str: "0"}
+	}
+
+	return ss
 }
 
-func resourceSplunkSearchRead(d *schema.ResourceData, m interface{}) error {
+func resourceSearchCreate(d *schema.ResourceData, m interface{}) error {
 	sc := client()
-	_, err := sc.GetSearch(d.Get("name").(string))
+	ss := search(d)
+
+	ns, err := sc.NewSearch(ss)
+	d.SetId(ns["name"].Str)
 	return err
 }
 
-func resourceSplunkSearchUpdate(d *schema.ResourceData, m interface{}) error {
+func resourceSearchRead(d *schema.ResourceData, m interface{}) error {
+	sc := client()
+	ss, err := sc.GetSearch(d.Get("name").(string))
+	if err != nil {
+		serr, ok := err.(spk.SError)
+		if !ok {
+			return err
+		}
+		if serr.StatusCode == 404 {
+			d.SetId("")
+			return nil
+		}
+	}
+
+	d.Set("search", ss["search"].Str)
+	d.Set("description", ss["description"].Str)
+	d.Set("disabled", ss["disabled"].Str == "1")
+
 	return nil
 }
 
-func resourceSplunkSearchDelete(d *schema.ResourceData, m interface{}) error {
+func resourceSearchUpdate(d *schema.ResourceData, m interface{}) error {
+	sc := client()
+	// I could create a search with *only* the updated fields, but it's 1 call either way
+	ss := search(d)
+	_, err := sc.UpdateSearch(ss)
+	return err
+}
+
+func resourceSearchDelete(d *schema.ResourceData, m interface{}) error {
 	sc := client()
 	_, err := sc.DeleteSearch(d.Get("name").(string))
 	return err
